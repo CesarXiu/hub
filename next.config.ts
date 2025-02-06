b30c9cd839a5c10f101264779c27e82cbd23451e
CesarXiu <cesarxiu504@gmail.com>
2025-02-06 13:59:45 -0500

    path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/hub/out',
   skipTrailingSlashRedirect: true,
   trailingSlash: true,
   output: 'export',
